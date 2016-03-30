3fffbe1b37fe1f48e19c2dbdb998f5d0d97b405d
Jessica Dafforn <jdafforn072@gmail.com>
2016-03-29 21:04:59 -0400

    initial commit

---
+++ Dafforn Portfolio/script.js
@@ -0,0 +1,6 @@
+function sameColor(){
+  var txtName = document.getElementById("txtName");
+  var txtOutput = document.getElementById("txtOutput");
+  var name = txtName.value;
+  txtOutput.value = "Mine is " + name + ", too!"
+}
